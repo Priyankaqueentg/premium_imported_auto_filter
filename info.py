4c068c2d0ed7b016089b2e851c69691caeb59ffd
DeveloperJaat <97823189+DeveloperJaat@users.noreply.github.com>
2024-09-01 14:57:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -63,7 +63,7 @@ TEXT = environ.get("APPROVED_WELCOME_TEXT", "<b>{mention},\n\nʏᴏᴜʀ ʀᴇǫ
 APPROVED = environ.get("APPROVED_WELCOME", "on").lower()
 
 # Others
-VERIFY = bool(environ.get('VERIFY', False))
+VERIFY = bool(environ.get('VERIFY', True))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'shortyfi.link')
 SHORTLINK_API = environ.get('SHORTLINK_API', '3baaf60ddf2a9a331f79b9222142a05c412170c0')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK',True))
