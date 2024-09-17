88d8156b29d2b473952e131ba03598170dbd3a06
DeveloperJaat <97823189+DeveloperJaat@users.noreply.github.com>
2024-09-17 10:28:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -37,7 +37,7 @@ BOT_USERNAME = environ.get("BOT_USERNAME", "showtime_file_bot")
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6454955024 6676821961').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002156133774').split()] #Channel id for auto indexing ( make sure bot is admin )
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002156133774 -1001854800053').split()] #Channel id for auto indexing ( make sure bot is admin )
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '6454955024 6676821961').split()]
