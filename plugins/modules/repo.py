586cd378673b817390c9e8a22e31c8368577625b
Oo <officialwildcash4@gmail.com>
2024-09-26 23:39:31 +0530

    Update repo.py

--- plugins/modules/repo.py
+++ plugins/modules/repo.py
@@ -31,7 +31,7 @@ async def git(Kashmira, message):
 
 <b>Open Issues :</b> <i>{qw.get("open_issues")}</i>
 
-<b>Powered by :</b> @evil00000
+<b>Powered by :</b> @Kalmoviesking
 """
         if qw.get("description"):
             txt += f'<b>Description :</b> <code>{qw.get("description")}</code>'
