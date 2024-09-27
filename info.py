ae8e33ec5cde3ede6c90089f37e1893806b37c4d
Oo <officialwildcash4@gmail.com>
2024-09-27 12:20:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -63,7 +63,7 @@ TEXT = environ.get("APPROVED_WELCOME_TEXT", "<b>{mention},\n\nʏᴏᴜʀ ʀᴇǫ
 APPROVED = environ.get("APPROVED_WELCOME", "on").lower()
 
 # Others
-VERIFY = bool(environ.get('VERIFY', True))
+VERIFY = bool(environ.get('VERIFY', False))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'shortyfi.link')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'd1ab14b4000c8e5f29fc2b621d4a2952a6d99e91')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK',True))
