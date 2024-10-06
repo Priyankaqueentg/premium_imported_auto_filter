1023b2786eea88f6c7bd4e74c28a68ec05ce52dc
Oo <officialwildcash4@gmail.com>
2024-10-06 08:32:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -111,8 +111,8 @@ else:
     ON_HEROKU = False
 BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', ''))
 FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://{}/".format(FQDN, PORT)
+URL = "https://polite-octopus-ajb-8e01fc1e.koyeb.app/".format(FQDN) if ON_HEROKU or NO_PORT else \
+    "https://polite-octopus-ajb-8e01fc1e.koyeb.app/".format(FQDN, PORT)
 SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
 WORKERS = int(environ.get('WORKERS', '4'))
 SESSION_NAME = str(environ.get('SESSION_NAME', 'JisshuBot'))
@@ -127,9 +127,9 @@ else:
     ON_HEROKU = False
 HAS_SSL=bool(getenv('HAS_SSL',True))
 if HAS_SSL:
-    URL = "https://{}/".format(FQDN)
+    URL = "https://polite-octopus-ajb-8e01fc1e.koyeb.app/".format(FQDN)
 else:
-    URL = "http://{}/".format(FQDN)
+    URL = "https://polite-octopus-ajb-8e01fc1e.koyeb.app/".format(FQDN)
 
 
 # Online Stream and Download
